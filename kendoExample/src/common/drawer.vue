05e7dc60a2801475442e4c4b1a25b92b8cd47ed4
신재용 <sls0263@gmail.com>
2023-12-21 16:41:40 +0900

    2023.12.21 메뉴 나오는 위치 수정

--- kendoExample/src/common/drawer.vue
+++ kendoExample/src/common/drawer.vue
@@ -102,50 +102,51 @@ export default {
   
 <style lang="scss" scoped>
 .drawer {
-visibility: hidden;
+    visibility: hidden;
 
-&.is-visible {
-    visibility: visible;
-}
-
-&.is-open {
-    .drawer__overlay {
-    opacity: 0.5;
+    &.is-visible {
+        visibility: visible;
     }
 
-    .drawer__content {
-    transform: translateX(0);
+    &.is-open {
+        .drawer__overlay {
+        opacity: 0.5;
+        }
+
+        .drawer__content {
+        transform: translateX(0);
+        }
     }
-}
 
-&__overlay {
-    position: fixed;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    left: 0;
-    width: 100%;
-    z-index: 200;
-    opacity: 0;
-    transition-property: opacity;
-    background-color: #000000;
-    user-select: none;
-}
+    &__overlay {
+        position: fixed;
+        top: 0;
+        right: 0;
+        bottom: 0;
+        left: 0;
+        width: 100%;
+        z-index: 200;
+        opacity: 0;
+        transition-property: opacity;
+        background-color: #000000;
+        user-select: none;
+    }
 
-&__content {
-    position: fixed;
-    top: 0;
-    right: 0;
-    bottom: 0;
-    height: 100%;
-    width: 100%;
-    z-index: 9999;
-    overflow: auto;
-    transition-property: transform;
-    display: flex;
-    flex-direction: column;
-    transform: translateX(100%);
-    box-shadow: 0 2px 6px #777;
-}
+    &__content {
+        position: fixed;
+        top: 0;
+        // 메뉴 오른쪽에 나오게 함
+        left: 0px;
+        bottom: 0;
+        height: 100%;
+        width: 100%;
+        z-index: 9999;
+        overflow: auto;
+        transition-property: transform;
+        display: flex;
+        flex-direction: column;
+        transform: translateX(-100%);
+        box-shadow: 0 2px 6px #777;
+    }
 }
 </style>
