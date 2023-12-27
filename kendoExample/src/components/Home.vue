c7d7628e895e8c6fa95c3446a3b415b57c61a65f
신재용 <sls0263@gmail.com>
2023-12-27 13:53:37 +0900

    2023.12.27 메인화면 글자 변경

--- kendoExample/src/components/Home.vue
+++ kendoExample/src/components/Home.vue
@@ -2,8 +2,8 @@
         <div class="container mt-5">
             <div class='row'>
                 <div class='col-12'>
-                    <h1 class='welcome mb-0'>Welcome to Kendo UI for Vue</h1>
-                    <h2 class='sub-header mt-0'>메인화면이에요</h2>
+                    <h1 class='welcome mb-0'>Welcome to Admin Page for Vue</h1>
+                    <h2 class='sub-header mt-0'>메인화면</h2>
                 </div>
             </div>
             <div class='row'>
